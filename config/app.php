27eba6389d3d7799f9ce1347d78c3ea5b92da268
Ilham-r <66206508+Ilham-r@users.noreply.github.com>
2021-05-16 00:44:59 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://interior-design1.herokuapp.com),
 
     'asset_url' => env('ASSET_URL', null),
 
