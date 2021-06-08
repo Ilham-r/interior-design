5c87d516a8d78bb7cd51b7c5e3c235c4edb389b6
Ilham-r <66206508+Ilham-r@users.noreply.github.com>
2021-06-08 23:22:18 +0100

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
        <img  class="col-lg-9 col-md-12 " src="/images/home.jpg" alt="">
 
         <div >
-            <h1>We will change your vision</h1>
+            <h1>Lorem ipsum dolor</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis cum expedita quasi fuga quas commodi similique labore aperiam, suscipit laborum ea ducimus deleniti cupiditate numquam aliquid inventore, corporis veritatis? Error.</p>
         </div>
     </div>
